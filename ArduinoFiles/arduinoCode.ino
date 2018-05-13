3212e941974500b3b5fd3acd94089db0144b6329
Axviel <axviel.benitez@upr.edu>
2018-05-13 02:23:08 -0400

    Error while uploading to the arduino

--- ArduinoFiles/arduinoCode.ino
+++ ArduinoFiles/arduinoCode.ino
@@ -42,30 +42,6 @@ void theaterChase(uint32_t c, uint8_t wait) {
     } 
   } 
 }
-uint32_t Wheel_1(byte WheelPos) {
-   WheelPos = 255 - WheelPos;
-   if (WheelPos < 85)
-       {
-       return strip.Color(0- WheelPos * 3, 0, WheelPos * 3);
-       }   if (WheelPos < 170) {
-       WheelPos -= 85;
-       return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-   }
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 0 - WheelPos * 3, 0);
-}
-void rainbow_1(uint8_t wait) {
-uint16_t i, j;
-
-for(j=0; j<256; j++) {
-for(i=0; i<strip.numPixels(); i++) {
-strip.setPixelColor(i, Wheel_1((i+j) & 255));
-}
-strip.show();
-delay(wait);
-}
-}
 void loop() {
-  colorWipe(strip.Color(0,0, 255), 50000);
-  rainbow(3000);
+  colorWipe(strip.Color(0,0, 255), 5000);
 }
